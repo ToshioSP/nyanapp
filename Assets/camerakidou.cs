ae94d16d505ea0e59bf06c998a6fbbdfb202b5e3
toshiosp <hustlerkind1@gmail.com>
2016-08-29 18:45:24 +0900

    カメラ

--- Assets/camerakidou.cs
+++ Assets/camerakidou.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.IO;
 public class camerakidou : MonoBehaviour {
 
     // Use this for initialization
@@ -12,6 +12,8 @@ public class camerakidou : MonoBehaviour {
     public bool flaga = false;
     public void Camerakidou()
     {
+        GameObject.Find("NonActObj").SetActive(false);
+
         flaga = true;
         WebCamDevice[] devices = WebCamTexture.devices;
         webcamTexture = new WebCamTexture(devices[0].name, Width, Height, FPS);
@@ -27,19 +29,58 @@ public class camerakidou : MonoBehaviour {
         webcamTexture.Play();
 
     }
-    // Update is called once per frame
-    void Update () {
-        /*
-        if (flaga)
+
+    public void Shot()
+    {
+
+        string format = "yyyy-MM-dd-HH-mm-ss";
+        string fileName = System.DateTime.Now.ToString(format) + ".png";
+
+        //スクリーンショット撮影(保存先はApplication.persistentDataPath直下)
+        Application.CaptureScreenshot(fileName);
+
+
+        string filePath = "";
+
+        // select platform
+        switch (Application.platform)
         {
-            Texture2D texture = new Texture2D(webcamTexture.width, webcamTexture.height);
-            GameObject.Find("Panel").GetComponent<Renderer>().material.mainTexture = texture;
-
-            texture.SetPixels32(color32);
-            texture.Apply();
-
+            case RuntimePlatform.IPhonePlayer:
+                filePath = Application.persistentDataPath + "/" + fileName;
+                break;
+            case RuntimePlatform.Android:
+                filePath = Application.persistentDataPath + "/" + fileName;
+                break;
+            default:
+                filePath = Application.persistentDataPath + "/" + fileName;
+//                filePath = fileName;
+                break;
         }
-        */
+        //ファイルの保存を待つ処理
+        StartCoroutine(ImageCheck(filePath));
+        print("撮影完了");
+
     }
 
+    IEnumerator ImageCheck(string _filePath){
+            while (File.Exists(_filePath) == false)
+            {
+                yield return new WaitForSeconds(0.2f); //
+            }
+            Debug.Log("OK"); ;
+    }
+        // Update is called once per frame
+        void Update () {
+        /*
+                if (flaga)
+                {
+                    Texture2D texture = new Texture2D(webcamTexture.width, webcamTexture.height);
+                    GameObject.Find("Panel").GetComponent<Renderer>().material.mainTexture = texture;
+
+                    texture.SetPixels32(color32);
+                    texture.Apply();
+
+                }
+            */
+    }
 }
